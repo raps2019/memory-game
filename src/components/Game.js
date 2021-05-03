1732b3d612f04d34a538b2d8dba383f517139575
Douglas Colaco <douglascolaco@gmail.com>
2021-05-03 10:55:15 -0600

    Created new file for cards array

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,12 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import blazersImg from '../images/blazers.png'
-import bucksImg from '../images/bucks.png'
-import bullsImg from '../images/bulls.png'
-import cavsImg from '../images/cavs.png'
-import celticsImg from '../images/celtics.png'
-import clippersImg from '../images/clippers.png'
-import grizzliesImg from '../images/grizzlies.png'
-import hawksImg from '../images/hawks.png'
+import teamCardsArray from './teamCardsArray'
 import GameBoard from './GameBoard'
 import ScoreBoard from './ScoreBoard'
 
@@ -17,61 +10,19 @@ function Game() {
   //Create a function to randomize array. run it everytime we set state for cards
   //Everytime card is clicked, randomize array. 
 
-  const [ cards, setCards ] = useState(
-    [
-      {
-        id: 1,
-        clicked: false,
-        name: "Portland Trailblazers",
-        image: blazersImg,
-      },
-      {
-        id: 2,
-        clicked: false,
-        name: "Milwaukee Bucks",
-        image: bucksImg
-      },
-      {
-        id: 3,
-        clicked: false,
-        name: "Chicago Bulls",
-        image: bullsImg
-      },
-      {
-        id: 4,
-        clicked: false,
-        name: "Cleveland Cavaliers",
-        image: cavsImg
-      },
-      {
-        id: 5,
-        clicked: false,
-        name: "Boston Celtics",
-        image: celticsImg
-      },
-      {
-        id: 6,
-        clicked: false,
-        name: "Los Angeles Clippers",
-        image: clippersImg
-      },
-      {
-        id: 7,
-        clicked: false,
-        name: "Memphis Grizzlies",
-        image: grizzliesImg
-      },
-      {
-        id: 8,
-        clicked: false,
-        name: "Atlanta Hawks",
-        image: hawksImg
-      }
-    ]
-  )
-
   const [ score, setScore ] = useState(0)
   const [ highScore, setHighScore ] = useState(0)
+
+  const shuffleArray = (array) => {
+    const arrayCopy = array.slice(0)
+    for ( let i = arrayCopy.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]]
+    }
+    return arrayCopy;
+  }
+
+  const [ cards, setCards ] = useState(shuffleArray(teamCardsArray))
 
   const handleCardClick = (id) => {
 
@@ -84,11 +35,11 @@ function Game() {
 
   const advanceGame = (id) => {
     setCards(
-      cards.map( item => 
+      shuffleArray(cards.map( item => 
         item.id === id
         ? {...item, clicked: true}
         : item
-      )
+      ))
     )
     setScore( prevScore => (
       prevScore + 1
---
+++ src/components/teamCardsArray.js
@@ -0,0 +1,62 @@
+import blazersImg from '../images/blazers.png'
+import bucksImg from '../images/bucks.png'
+import bullsImg from '../images/bulls.png'
+import cavsImg from '../images/cavs.png'
+import celticsImg from '../images/celtics.png'
+import clippersImg from '../images/clippers.png'
+import grizzliesImg from '../images/grizzlies.png'
+import hawksImg from '../images/hawks.png'
+
+
+const teamCardsArray = [
+  {
+    id: 1,
+    clicked: false,
+    name: "Portland Trailblazers",
+    image: blazersImg,
+  },
+  {
+    id: 2,
+    clicked: false,
+    name: "Milwaukee Bucks",
+    image: bucksImg
+  },
+  {
+    id: 3,
+    clicked: false,
+    name: "Chicago Bulls",
+    image: bullsImg
+  },
+  {
+    id: 4,
+    clicked: false,
+    name: "Cleveland Cavaliers",
+    image: cavsImg
+  },
+  {
+    id: 5,
+    clicked: false,
+    name: "Boston Celtics",
+    image: celticsImg
+  },
+  {
+    id: 6,
+    clicked: false,
+    name: "Los Angeles Clippers",
+    image: clippersImg
+  },
+  {
+    id: 7,
+    clicked: false,
+    name: "Memphis Grizzlies",
+    image: grizzliesImg
+  },
+  {
+    id: 8,
+    clicked: false,
+    name: "Atlanta Hawks",
+    image: hawksImg
+  }
+]
+
+export default teamCardsArray
